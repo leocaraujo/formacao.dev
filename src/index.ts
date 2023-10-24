1ac5aefbdd25386b192efa75f696fee5515835fd
leocaraujo <leo.carvalho.araujo@gmail.com>
2023-10-24 11:33:21 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import "./style.css";
+
+class Produto {
+  constructor(
+    readonly id: number,
+    readonly nome: string,
+    readonly preco: number,
+    readonly desc: number
+  ) {}
+}
+
+export {};
